c56315be646bb87557390ef7e09847a4826d8337
PauRaset <94690926+PauRaset@users.noreply.github.com>
2026-02-07 19:49:43 +0100

    Clean up payments.js by removing comments
    Removed commented-out code and cleaned up the file.

--- borrar/routes/payments.js
+++ borrar/routes/payments.js
@@ -1,4 +1,4 @@
-/*// routes/payments.js
+// routes/payments.js
 const express = require('express');
 const Stripe = require('stripe');
 const Order = require('../models/Order');
@@ -327,10 +327,10 @@ router.get('/direct/:eventId', async (req, res) => {
     }
   });
 
-module.exports = router;*/
+module.exports = router;
 
 
-// routes/payments.js
+/*// routes/payments.js
 const express = require('express');
 const Stripe = require('stripe');
 const Order = require('../models/Order');
@@ -677,4 +677,4 @@ router.get('/direct/:eventId', async (req, res) => {
     }
   });
 
-module.exports = router;
+module.exports = router;*/
